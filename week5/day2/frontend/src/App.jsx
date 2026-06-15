ccdb5a9761027dda99ca1811fc54c0ea51c0300c
shakerbr <shbhky@gmail.com>
2026-06-15 11:03:18 +0300

    Include tailwind and add some style to the app

--- week5/day2/frontend/src/App.jsx
+++ week5/day2/frontend/src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <div className="text-[#fff] text-center text-9xl font-black bg-[#aaaaff20] m-10 rounded-2xl">Hey!</div>
+      <div className="p-4 mb-4 text-sm text-[#50a2ff] rounded-xl bg-[#172455] m-8" role="alert">
+        <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
+      </div>
+    </>
+  );
 }
 
 export default App
