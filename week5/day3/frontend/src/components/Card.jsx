10953eb37998883910b1d35f3df67182faa52d33
shakerbr <shbhky@gmail.com>
2026-06-18 16:31:58 +0300

    Add animation on hover

--- week5/day3/frontend/src/components/Card.jsx
+++ week5/day3/frontend/src/components/Card.jsx
@@ -16,7 +16,7 @@ function Card({children, name, age, department, city }) {
     ;
 
     return (
-        <div className="font-medium text-white min-w-90 min-h-50 p-6 rounded-2xl bg-[#007fffcc] shadow-md shadow-blue-500/50">
+        <div className="font-medium text-white min-w-90 min-h-50 p-6 rounded-2xl bg-[#007fffcc] shadow-md shadow-blue-500/50 transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
             <div className="flex flex-row">
                 <div className="flex flex-col justify-center w-18 h-18 bg-white rounded-2xl">
                     <p className='text-center text-[#007fff] font-black text-2xl'>{initials}</p>
