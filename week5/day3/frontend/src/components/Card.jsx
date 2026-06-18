1e16d961d224b303e623eb9da220cb99e1bd1944
shakerbr <shbhky@gmail.com>
2026-06-18 09:58:58 +0300

    Initialize day3 project

---
+++ week5/day3/frontend/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="text-4xl text-center m-30 p-20 font-black bg-[#007fffcc] rounded-xl text-white shadow-md shadow-blue-500/50">
+        <h1>Hey Component!</h1>
+    </div>
+  )
+}
+
+export default Card
