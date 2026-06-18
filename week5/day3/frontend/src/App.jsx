b453297b578cdf1cc3dbef2c60f0fe56351a4647
shakerbr <shbhky@gmail.com>
2026-06-18 12:24:57 +0300

    Finalized the card

--- week5/day3/frontend/src/App.jsx
+++ week5/day3/frontend/src/App.jsx
@@ -3,9 +3,25 @@ import Card from "./components/Card";
 function App() {
   return (
     <>
-      <div>
-        <Card name="Component" />
-      </div>
+      {/* <Header/> */}
+      <main className="flex flex-col md:flex-row gap-4 justify-center items-center">
+
+        <Card
+          name="Conquerer mateen"
+          age={5}
+          dept="Engineering"
+          city="Duhok"
+        />
+
+        <Card
+          name="Lord Snow"
+          age={7}
+          dept="Science"
+          city="Duhok"
+        > Test </Card>
+
+      </main>
+      {/* <Footer/> */}
     </>
   );
 }
--- week5/day3/frontend/src/components/Card.jsx
+++ week5/day3/frontend/src/components/Card.jsx
@@ -1,11 +1,52 @@
 import React from 'react'
 
-const Card = ({name}) => {
-  return (
-    <div className="text-4xl text-center m-30 p-20 font-black bg-[#007fffcc] rounded-xl text-white shadow-md shadow-blue-500/50">
-        <h1>Hey {name}!</h1>
-    </div>
-  )
+function Card({children, name, age, dept, city }) {
+    const initials = name 
+        .split(" ") 
+        .map((n) => n[0]) 
+        .join("") 
+        .slice(0, 2) 
+        .toUpperCase()
+    ;
+
+    const capName = name
+        .split(" ")
+        .map(w => w.charAt(0).toUpperCase() + w.slice(1))
+        .join(" ")
+    ;
+
+    return (
+        <div className="text-center p-6 font-medium bg-[#007fffcc] rounded-2xl text-white shadow-md shadow-blue-500/50 w-120 h-50">
+            <div className="upper flex flex-row">
+                <div className="profile-image bg-white rounded-2xl w-18 h-18 flex flex-col item-center justify-center">
+                    <p className='text-[#007fff] font-black text-2xl'>{initials}</p>
+                </div>
+                <div className="info px-4 text-left">
+                    <h3 className='font-black text-xl'>{capName}</h3>
+                    <p>{dept}</p>
+                </div>
+            </div>
+            <hr className="my-4" />
+            <div className="lower flex flex-col">
+                <div className="detail flex justify-between">
+                    <div className="title text-left pl-4">
+                        <p>Age:</p>
+                    </div>
+                    <div className="value text-right pr-4">
+                        <p>{age}</p>
+                    </div>
+                </div>
+                <div className="detail flex justify-between">
+                    <div className="title text-left pl-4">
+                        <p>City:</p>
+                    </div>
+                    <div className="value text-right pr-4">
+                        <p>{city}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Card
