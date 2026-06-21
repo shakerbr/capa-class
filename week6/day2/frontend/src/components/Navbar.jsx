5073fa87b912e5dc0152ce43041712664bc57161
shakerbr <shbhky@gmail.com>
2026-06-21 12:00:54 +0300

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added main application structure with routing (Home, About, Contact pages)
    - Implemented Navbar component for navigation
    - Included SVG icons for branding and functionality
    - Set up global styles with Tailwind CSS
    - Configured Vite for development and build processes
    - Added placeholder images and assets for future use

---
+++ week6/day2/frontend/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className='flex block border-2 rounded-full justify-center gap-6 text-xl font-bold p-2 m-4'>
+        <Link 
+            to="/"
+            className='transition-all duration-300 hover:underline' 
+        >
+            Home
+        </Link>
+        <Link 
+            to="/about"
+            className='transition-all duration-300 hover:underline' 
+        >
+            About
+        </Link>
+        <Link 
+            to="/contact"
+            className='transition-all duration-300 hover:underline' 
+        >
+            Contact
+        </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
